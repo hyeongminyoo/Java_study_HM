051627cd8ea97cccd906f4855d3cd0f23c7e5a10
hyeongminyoo <dbgudals2004@gmail.com>
2022-08-25 10:33:44 +0900

    paging finish

--- src/main/java/com/hm/start/util/Pager.java
+++ src/main/java/com/hm/start/util/Pager.java
@@ -22,6 +22,11 @@ public class Pager {
 	private Long perPage;
 	private Long perBlock;
 	
+	//이전 블럭의 유무
+	private boolean pre;
+	//다음 블럭의 유무
+	private boolean next;
+	
 	public Pager() {
 		this.perPage=10L;
 		this.perBlock=5L;
@@ -37,35 +42,55 @@ public class Pager {
 	
 	//2. JSP에서 사용할 값을 계산
 	public void getNum(Long totalCount) throws Exception{
-		//totalCount로 totalPage 구하기
+		//1.totalCount로 totalPage 구하기
 		Long totalPage = totalCount/this.getPerPage();
 		if(totalCount/this.getPerPage() != 0) {
 			totalPage++;
 		}
 		
-		//totalPage로 totalBlock 구하기
+		//1.1 totalPage보다 page가 더 클 경우
+		if(this.getPage()>totalPage) {
+			this.setPage(totalPage);
+		}
+		
+		
+		//2.totalPage로 totalBlock 구하기
 		Long totalBlock = totalPage/this.getPerBlock();
 		if(totalPage/this.getPerBlock() != 0) {
 			totalBlock++;
 		}
 		
-		//page 번호로 현재 Block 번호 찾기
+		//3.page 번호로 현재 Block 번호 찾기
 		Long curBlock = this.getPage()/this.getPerBlock();
 		
 		if(this.getPage()%this.getPerBlock() != 0) {
 			curBlock++;
 		}
 		
-		// curBlock 으로 시작번호와 끝 번호 알아오기
+		
+		//4. curBlock 으로 시작번호와 끝 번호 알아오기
 		
 		this.startNum = (curBlock-1)*this.getPerBlock()+1;
 		this.lastNum = curBlock*this.getPerBlock();
 		
+		//5. curBlock 이 totalBlock 과 같을 때
+		if(curBlock == totalBlock) {
+			this.lastNum = totalPage;
+		}
+		
+		//6. 이전, 다음 블럭의 유무
+		if(curBlock>1) {
+			pre = true;
+		}
+		
+		if(curBlock<=totalBlock) {
+			next = true;
+		}
 		
 	}
 	
 	public Long getPage() {
-		if(this.page==null) {
+		if(this.page==null || this.page <= 0) {
 			this.page=1L;
 		}
 		return page;
@@ -115,6 +140,26 @@ public class Pager {
 
 	public void setPerBlock(Long perBlock) {
 		this.perBlock = perBlock;
+	}
+
+
+	public boolean isPre() {
+		return pre;
+	}
+
+
+	public void setPre(boolean pre) {
+		this.pre = pre;
+	}
+
+
+	public boolean isNext() {
+		return next;
+	}
+
+
+	public void setNext(boolean next) {
+		this.next = next;
 	}
 	
 	
